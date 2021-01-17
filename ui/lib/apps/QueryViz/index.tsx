446f7c8af1987d6f041add68d57ac0a35fc17b83
Zheng Xiangsheng <hundundm@gmail.com>
2021-01-17 12:57:59 +0800

    fix theme
    Signed-off-by: Zheng Xiangsheng <hundundm@gmail.com>

--- ui/lib/apps/QueryViz/index.tsx
+++ ui/lib/apps/QueryViz/index.tsx
@@ -277,7 +277,7 @@ function App() {
                 type="primary"
                 htmlType="submit"
                 icon={<CaretRightOutlined />}
-                loading={isRunning}
+                disabled={isRunning}
               >
                 {t('queryviz.run')}
               </Button>
